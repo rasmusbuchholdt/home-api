181eabb7ec359791345d2d0712ac6b1f968c5187
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2020-12-03 23:56:25 +0100

    Add Sonos handler

---
+++ src/models/sonos/sonos-device.ts
@@ -0,0 +1,25 @@
+export interface Endpoints {
+    transport: string;
+    rendering: string;
+    device: string;
+}
+
+export interface Spotify {
+    region: string;
+}
+
+export interface Options {
+    endpoints: Endpoints;
+    spotify: Spotify;
+}
+
+export interface Events {
+}
+
+export interface SonosDevice {
+    host: string;
+    port: number;
+    options: Options;
+    _events: Events;
+    _eventsCount: number;
+}
---
+++ src/sonos.ts
@@ -0,0 +1,32 @@
+import { SonosDevice } from './models/sonos/sonos-device';
+
+let { Sonos } = require('sonos')
+let DeviceDiscovery = require('sonos').AsyncDeviceDiscovery;
+
+export class SonosHandler {
+
+    private sonos: any;
+
+    constructor(host?: string) {
+        if (host) {
+            this.sonos = new Sonos(host);
+        } else {
+            this.getSonos();
+        }
+    }
+
+    private getSonos() {
+        let discovery = new DeviceDiscovery();
+        discovery.discover().then((device: SonosDevice) => {
+            this.sonos = new Sonos(device.host);
+        })
+    }
+
+    pause(): void {
+        this.sonos.pause();
+    }
+
+    resume(): void {
+        this.sonos.play();
+    }
+}
