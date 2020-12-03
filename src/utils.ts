2dde375a974cf3ac11f3f9e8639c2ea0cf7a7d84
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2020-12-03 14:37:31 +0100

    New clamp helper function

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,10 @@ export function normalize(value: number, min: number, max: number): number {
     return (value - min) / (max - min) * 100;
 }
 
+export function clamp(value: number, min: number, max: number) {
+    return value > max ? max : value < min ? min : value;
+}
+
 export function randomString(length: number): string {
     let result: string = "";
     let possible: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
