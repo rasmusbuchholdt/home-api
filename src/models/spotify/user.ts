4ba5c7cca2cb60a416b982c029cc6dc8973e0779
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2020-12-06 09:52:40 +0100

    Indention

--- src/models/spotify/user.ts
+++ src/models/spotify/user.ts
@@ -1,34 +1,34 @@
 export interface ExplicitContent {
-    filter_enabled: boolean;
-    filter_locked: boolean;
+  filter_enabled: boolean;
+  filter_locked: boolean;
 }
 
 export interface ExternalUrls {
-    spotify: string;
+  spotify: string;
 }
 
 export interface Followers {
-    href?: any;
-    total: number;
+  href?: any;
+  total: number;
 }
 
 export interface Image {
-    height?: any;
-    url: string;
-    width?: any;
+  height?: any;
+  url: string;
+  width?: any;
 }
 
 export interface SpotifyUser {
-    country: string;
-    display_name: string;
-    email: string;
-    explicit_content: ExplicitContent;
-    external_urls: ExternalUrls;
-    followers: Followers;
-    href: string;
-    id: string;
-    images: Image[];
-    product: string;
-    type: string;
-    uri: string;
+  country: string;
+  display_name: string;
+  email: string;
+  explicit_content: ExplicitContent;
+  external_urls: ExternalUrls;
+  followers: Followers;
+  href: string;
+  id: string;
+  images: Image[];
+  product: string;
+  type: string;
+  uri: string;
 }
