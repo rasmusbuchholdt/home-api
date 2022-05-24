daf21838a44185b4302b690c53889244d94608ec
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2022-05-24 12:02:39 +0200

    Set up hue is now a separate script
    https://www.npmjs.com/package/node-hue-api

--- src/hue.ts
+++ src/hue.ts
@@ -98,37 +98,4 @@ export class HueHandler {
       enabled ? this.disableLight(lightId) : this.enableLight(lightId);
     });
   }
-
-  async discoverAndCreateUser() {
-    const ipAddress = await this.getBridgeIp();
-
-    // Create an unauthenticated instance of the Hue API so that we can create a new user
-    const unauthenticatedApi = await v3.api.createLocal(ipAddress).connect();
-
-    let createdUser;
-    try {
-      createdUser = await unauthenticatedApi.users.createUser('home-api', 'pi');
-      console.log('*******************************************************************************\n');
-      console.log('User has been created on the Hue Bridge. The following username can be used to\n' +
-        'authenticate with the Bridge and provide full local access to the Hue Bridge.\n' +
-        'YOU SHOULD TREAT THIS LIKE A PASSWORD\n');
-      console.log(`Hue Bridge User: ${createdUser.username}`);
-      console.log(`Hue Bridge User Client Key: ${createdUser.clientkey}`);
-      console.log('*******************************************************************************\n');
-
-      // Create a new API instance that is authenticated with the new user we created
-      const authenticatedApi = await v3.api.createLocal(ipAddress).connect(createdUser.username);
-
-      // Do something with the authenticated user/api
-      const bridgeConfig = await authenticatedApi.configuration.getConfiguration();
-      console.log(`Connected to Hue Bridge: ${bridgeConfig.name} :: ${bridgeConfig.ipaddress}`);
-
-    } catch (err) {
-      if (err.getHueErrorType() === 101) {
-        console.error('The Link button on the bridge was not pressed. Please press the Link button and try again.');
-      } else {
-        console.error(`Unexpected Error: ${err.message}`);
-      }
-    }
-  }
 }
